8ca164ba6dc676b1557ffbd2694dbdc00023aa42
Noah <noa316@naver.com>
2022-04-08 23:33:20 +0900

    [ADD] : 과제 프로젝트 생성

---
+++ Assignment/30th-assignment/30th-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30th-assignment
+//
+//  Created by Noah on 2022/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
