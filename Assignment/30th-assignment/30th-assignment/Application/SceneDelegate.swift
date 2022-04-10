46199fbfb721cadfffee9356c33af8de15ccdf2c
Noah <noa316@naver.com>
2022-04-11 00:54:55 +0900

    [DEL] : 사용하지 않는 코드 제거

--- Assignment/30th-assignment/30th-assignment/Application/SceneDelegate.swift
+++ Assignment/30th-assignment/30th-assignment/Application/SceneDelegate.swift
@@ -8,13 +8,10 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // window?.rootViewController = ViewControllerFactory.viewController(for: .login)
-        // guard let _ = (scene as? UIWindowScene) else { return }
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let navigationController = UINavigationController()
         let window = UIWindow(windowScene: windowScene)
