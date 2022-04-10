e57235f851f8006f7fee4e6baba29ed1f03af326
Noah <noa316@naver.com>
2022-04-10 18:35:11 +0900

    [FEAT] GreetingViewModel 구현

---
+++ Assignment/30th-assignment/30th-assignment/Presentation/LoginScene/ViewModel/GreetingViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  GreetingViewModel.swift
+//  30th-assignment
+//
+//  Created by Noah on 2022/04/10.
+//
+
+import Foundation
+
+struct GreetingViewModel {
+    struct Output {
+        @Observable var userIdentifier = ""
+        @Observable var userPassword = ""
+    }
+    
+    var output = Output()
+    weak var coordinator: LoginCoordinator?
+    
+    init(coordinator: LoginCoordinator) {
+        self.coordinator = coordinator
+        if let userIdentifier = UserInfo.shared.userId {
+            self.output.userIdentifier = userIdentifier
+        }
+    }
+    
+    func completeButtonDidTap() {
+        self.coordinator?.performTransition(to: .dismiss)
+    }
+}
