6a96948fbd0de1f925607c783acb023a67ab97ca
Noah <noa316@naver.com>
2022-04-10 18:34:53 +0900

    [FEAT] GreetingViewController 구현

---
+++ Assignment/30th-assignment/30th-assignment/Presentation/LoginScene/ViewController/GreetingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GreetingViewController.swift
+//  30th-assignment
+//
+//  Created by Noah on 2022/04/10.
+//
+
+import UIKit
+
+final class GreetingViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet private weak var greetingLabel: UILabel!
+    
+    // MARK: - property
+    
+    var greetingViewModel: GreetingViewModel?
+    
+    // MARK: - life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+    }
+    
+    // MARK: - func
+    
+    @IBAction private func completeButtonDidTap(_ sender: UIButton) {
+        greetingViewModel?.completeButtonDidTap()
+    }
+    
+    @MainActor private func bindViewModel() {
+        self.greetingViewModel?.output.$userIdentifier.bind(fire: true) { (value) in
+            self.greetingLabel.text = "\(value)님, Instagram에\n 오신 것을 환영합니다."
+        }
+    }
+}
