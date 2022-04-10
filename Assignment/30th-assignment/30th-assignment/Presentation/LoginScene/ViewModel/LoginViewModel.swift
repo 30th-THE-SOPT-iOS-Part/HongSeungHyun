bf5a97c586328fa865c49c77a41bbe47d6811b13
Noah <noa316@naver.com>
2022-04-10 18:33:26 +0900

    [FEAT] LoginViewModel 구현

--- Assignment/30th-assignment/30th-assignment/Presentation/LoginScene/ViewModel/LoginViewModel.swift
+++ Assignment/30th-assignment/30th-assignment/Presentation/LoginScene/ViewModel/LoginViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct LoginViewModel {
+    struct Input {
+        var userIdentifier: String?
+        var userPassword: String?
+    }
+    
+    var input: Input = Input()
+    weak var coordinator: LoginCoordinator?
+    
+    init(coordinator: LoginCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func loginButtonDidTap() {
+        UserInfo.shared.userId = self.input.userIdentifier
+        UserInfo.shared.userPassword = self.input.userPassword
+        self.coordinator?.performTransition(to: .greeting)
+    }
+    
+    func signUpButtonDidTap() {
+        self.coordinator?.performTransition(to: .inputUsername)
+    }
+}
