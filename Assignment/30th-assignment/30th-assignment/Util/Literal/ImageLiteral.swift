1cc53d5b67613ea82e7706213a0af4e62f500731
Noah <noa316@naver.com>
2022-04-23 02:53:22 +0900

    [ADD] : 2차과제 이미지 에셋 추가

--- Assignment/30th-assignment/30th-assignment/Util/Literal/ImageLiteral.swift
+++ Assignment/30th-assignment/30th-assignment/Util/Literal/ImageLiteral.swift
@@ -9,10 +9,18 @@ import UIKit
 
 enum ImageLiteral {
     static var btnBackBlack: UIImage { .load(named: "back_button")}
-
+    static var iconHome: UIImage { .load(named: "icn_home")}
+    static var iconHomeSelected: UIImage { .load(named: "icn_home_selected")}
+    static var iconProfile: UIImage { .load(named: "icn_profile")}
+    static var iconProfileSelected: UIImage { .load(named: "icn_profile_selected")}
+    static var iconReels: UIImage { .load(named: "icn_reels")}
+    static var iconReelsSelected: UIImage { .load(named: "icn_reels_selected")}
+    static var iconSearch: UIImage { .load(named: "icn_search")}
+    static var iconSearchSelected: UIImage { .load(named: "icn_search_selected")}
+    static var iconShop: UIImage { .load(named: "icn_shop")}
+    static var iconShopSelected: UIImage { .load(named: "icn_shop_selected")}
     static var iconPasswordHidden: UIImage { .load(named: "password hidden eye icon")}
     static var iconPasswordShown: UIImage { .load(named: "password shown eye icon")}
-
     static var imgInstagramLogo: UIImage { .load(named: "Instagram Black Logo")}
 }
 
